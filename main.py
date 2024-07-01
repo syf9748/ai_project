772474d5328f6cf89eb755baa5abecc32a131987
syf9748 <syf9748@gmail.com>
2024-07-01 15:24:08 +0800

    File upload & vector database

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import StreamingResponse, JSONResponse
 from pydantic import BaseModel
 import onnxruntime_genai as og
 import asyncio
 import time
+from sentence_transformers import SentenceTransformer
+from pymilvus import MilvusClient, DataType
+import uuid
+from unstructured.partition.pdf import partition_pdf
+from unstructured.chunking.title import chunk_by_title
+from unstructured.staging.base import convert_to_dataframe
+import json
+
 
 # define query format
 class Item(BaseModel):
@@ -14,36 +22,103 @@ model = og.Model('cuda-int4-rtn-block-32')
 tokenizer = og.Tokenizer(model)
 tokenizer_stream = tokenizer.create_stream()
 params = og.GeneratorParams(model)
-search_options = {'max_length':2048,'do_sample': True,'temperature':1.0} 
+search_options = {'max_length':2048} 
 params.set_search_options(**search_options)
-systemq = "You are a customer service manager. You should politely answer the question from the customer."
+systemq = "You are a customer service manager. You should use the information from context to answer customer's question."
+#chat_template = '<|user|>\n{input}<|end|>\n<|assistant|>'
 chat_template = '<|system|>\n{system}<|end|><|user|>\n{input}<|end|>\n<|assistant|>'
+
+#Load transformer model
+model_t = SentenceTransformer("all-MiniLM-L6-v2/")
+
+def generate_uuid():
+	id = int(uuid.uuid4())
+	id = str(id)[:10]
+	id = int(id)
+	return id
 
 app = FastAPI()
 
 # feed input into the model
-def model_process(query):
+async def model_process(query):
 	prompt = f'{chat_template.format(system=systemq,input=query)}'
 	input_tokens = tokenizer.encode(prompt)
 	params.input_ids = input_tokens
 	generator = og.Generator(model, params)
 	
-	output = []
+	#output = []
 	while not generator.is_done():
 		generator.compute_logits()
 		generator.generate_next_token()
 		new_token = generator.get_next_tokens()[0]
 		#store a list of tokens
-		output.append(tokenizer_stream.decode(new_token))
+		#output.append(tokenizer_stream.decode(new_token))
+		yield tokenizer_stream.decode(new_token)
 
-	return output
+	#return output
 
-@app.post("/items/")
+@app.post("/chat/")
 async def create_item(item: Item):
 	
 	query_dict = item.model_dump()
 	query = query_dict['query']
-	# needs to be returned in json format
-	output = {"response":model_process(query)}
+	vectors = model_t.encode(query)
 
-	return output
+	client = MilvusClient("./milvus_demo.db")
+	res = client.search(
+		collection_name="demo_collection",
+		data=[vectors],
+		limit=3,
+		output_fields=["text"],
+	)
+	result = ""
+	for i in res[0]:
+		result = result + " " + i["entity"]["text"]
+
+	new_query = "question: " + query + "\n context: " + result
+	client.close()
+	# needs to be returned in json format
+	#output = {"response":model_process(query)}
+
+	#return output
+	return StreamingResponse(model_process(new_query))
+
+@app.post("/file/")
+async def create_file(file: UploadFile):
+	
+	elements = partition_pdf(file=file.file,content_type="application/pdf", strategy ="fast")
+
+	chunks = chunk_by_title(elements,new_after_n_chars=1000,max_characters=1000)
+	df_chunk=convert_to_dataframe(chunks)
+	lines = df_chunk['text'].to_list()
+	
+	#lines = []
+	#for line in file.file:
+		#lines.append(line.decode("utf-8"))
+
+	vectors = model_t.encode(lines)
+
+	data = [ {"id":generate_uuid(), "vector": vectors[i,:], "text": lines[i], "subject": "history"} for i in range(len(vectors)) ]
+	
+	#create milvus database
+	client = MilvusClient("./milvus_demo.db")
+	client.drop_collection(collection_name="demo_collection")
+	client.create_collection(
+		collection_name="demo_collection",
+		dimension=384,  # The vectors we will use in this demo has 384 dimensions
+	)
+
+
+	res = client.insert(
+		collection_name="demo_collection",
+		data=data
+	)
+
+	# This will exclude any text in "history" subject despite close to the query vector.
+	
+	client.close()
+
+	return({"answer":"Upload successful"})
+
+
+
--- streamlit.py
+++ streamlit.py
@@ -3,16 +3,21 @@ import requests
 import json
 import time
 
-api_url = "http://127.0.0.1:8000/items/"
+api_url = "http://127.0.0.1:8000/"
 
 # sidebar
 with st.sidebar:
     upload_file = st.file_uploader("Drop your file")
-    st.header("File list:")
+    
     if "file" not in st.session_state:
         st.session_state.file = []
-    if upload_file is not None:
+    if upload_file is not None and upload_file.name not in st.session_state.file:
         st.session_state.file.append(upload_file.name)
+        #send file to the api
+        response = requests.post(api_url+"file/", files={"file":upload_file})
+        response_json = response.json()
+        st.write(response_json["answer"])
+    st.header("File list:")
     for file in st.session_state.file:
         st.write(file)
 
@@ -41,14 +46,14 @@ if prompt := st.chat_input("Say something"):
     with st.chat_message("assistant"):
 
         # Get response from FASTAPI
-        response = requests.post(api_url, json = {"query":prompt})#, stream=True)
+        response = requests.post(api_url+"chat/", json = {"query":prompt}, stream=True)
 
         if response.status_code == 200:
             #output = st.write_stream(response.iter_content())
             def generate_output(response):
-                for item in response.json()["response"]:
-                    yield item
-                    time.sleep(0.1)
+                for item in response:#.json()["response"]:
+                    yield item.decode("utf-8")
+                    #time.sleep(0.1)
             output = st.write_stream(generate_output(response))
         else:
             output = st.write("Error: " + str(response.status_code))
